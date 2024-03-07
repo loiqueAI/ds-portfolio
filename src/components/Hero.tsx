847640ccbc50306f0a16a4fb72ef8ba98b5fbfae
loique <>
2024-03-07 05:06:58 -0800

    Escape correction

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = (props: Props) => {
       <div className="w-[80%] grid grid-cols-1 mx-auto lg:grid-cols-2 gap-[3rem] h-[100%] items-center">
         <div>
           <h1 className="text-[35px] md:text-[50px] text-white font-bold">
-            HI, I'M <span className="text-yellow-400">Loique </span>
+            HI, I&apos;M <span className="text-yellow-400">Loique </span>
           </h1>
           <TextEffect />
           <p className="mt-[1.5rem] text-[18px] text-[#ffffff92]">
